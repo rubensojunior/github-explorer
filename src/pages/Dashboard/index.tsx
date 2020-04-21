702b6de3f5ee71be263535cf40641213f823aa03
rubensojunior <rubensojunior6@gmail.com>
2020-04-20 23:18:09 -0300

    Styling dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src={logoImg} alt="junior" />
+
+          <div>
+            <strong>SmileCode</strong>
+            <p>descricao do smilecode</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src={logoImg} alt="junior" />
+
+          <div>
+            <strong>SmileCode</strong>
+            <p>descricao do smilecode</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard
