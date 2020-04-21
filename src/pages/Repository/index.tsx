c6b24940572a567e19c798735513c92d65354126
rubensojunior <rubensojunior6@gmail.com>
2020-04-21 01:16:40 -0300

    implementing repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouteMatch, Link } from 'react-router-dom'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
 
 import { Header, RepositoryInfo, Issues } from './styles'
 
@@ -10,8 +11,46 @@ interface RepositoryParams {
   repository: string
 }
 
+interface Repository {
+  full_name: string
+  description: string
+  stargazers_count: number
+  forks_count: number
+  open_issues_count: number
+  owner: {
+    login: string
+    avatar_url: string
+  }
+}
+
+interface Issue {
+  id: number
+  title: string
+  html_url: string
+  user: {
+    login: string
+  }
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null)
+  const [issues, setIssues] = useState<Issue[]>([])
+
   const { params } = useRouteMatch<RepositoryParams>()
+
+  useEffect(() => {
+    const loadData = async (): Promise<void> => {
+      const [repositoryLoaded, issuesLoaded] = await Promise.all([
+        api.get(`repos/${params.repository}`),
+        api.get(`repos/${params.repository}/issues`),
+      ])
+
+      setRepository(repositoryLoaded.data)
+      setIssues(issuesLoaded.data)
+    }
+
+    loadData()
+  }, [params.repository])
 
   return (
     <>
@@ -22,40 +61,46 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </Header>
-
-      <RepositoryInfo>
-        <header>
-          <img src="" alt="" />
-          <div>
-            <strong>a configurar...</strong>
-            <p>a configurar..</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>a configurar..</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>a configurar..</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>a configurar..</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
 
       <Issues>
-        <Link to="fdfd">
-          <div>
-            <strong>a configurar..</strong>
-            <p>a configurar..</p>
-          </div>
+        {issues.map((issue) => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </Link>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   )
